1de6d40ab7d1189c83445848265ed88df802f7aa
saintjohnie <42511508+saintjohnie@users.noreply.github.com>
2020-06-03 10:03:54 +0100

    Create nwagboJohn.js
    This is a script containing my details

---
+++ scripts/nwagboJohn.js
@@ -0,0 +1,7 @@
+const details=(fullname, id, language)=>{
+  
+  return `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  
+}
+console.log(details
+("Nwagbo John", "HNG-01765", "Javascript"));
